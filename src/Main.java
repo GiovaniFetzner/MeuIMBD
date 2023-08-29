802c22ab7edab89b764d4a7c502deea3c8df8fa3
Giovani <giovanifetzner01@gmail.com>
2023-08-28 21:30:03 -0300

    Criando base do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.nio.file.FileSystemNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Repositorio Repositorio = new Repositorio();
+
+        ArquivoDigital ArquivoDigital = new ArquivoDigital(123, 2023, "Oppenheimer", "Crystopher Nolan",
+                "Cillian M.", Categoria.FILME);
+
+        ArquivoDigital ArquivoDigital = new ArquivoDigital(321,2023,"Ragnarok", "Desconhecido",
+                "Desconhecido", Categoria.SERIADO);
+
+
 
         }
 
